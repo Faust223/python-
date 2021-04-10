329cbb4a1173f8b63e2867212e1bb42d91bd7fa0
QuankeXu <39804104+Faust223@users.noreply.github.com>
2021-04-10 22:41:53 +0800

    Add files via upload

---
+++ 基础一/06-切片.py
@@ -0,0 +1,27 @@
+list1 = ['age',1,23.3,True]
+
+# 切片：[a:b:c] 取值范围：a-b（不包含b）c代表步长
+print(list1[0:3:2])
+msg = "helloword"
+print(msg[0:6:3])
+
+# 分段赋值
+a,b,c,d = 3,3,4,3
+print(a,b,c,d)
+
+# 连续等式
+f1 = f2 = f3 = f4 = 12
+print(f1,f2,f3,f4)
+
+# pass关键字
+if 5>3:
+    pass
+
+
+
+
+
+
+
+
+
